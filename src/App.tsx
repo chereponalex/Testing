713ba73b6f5b6185cb82bf0f74bb7ea988537edb
chereponalex <lexa_klav@mail.ru>
2024-06-02 21:06:52 +0300

    created ListPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-
+import "./index.css";
 import { HomePage } from "./pages/ListEntity/index";
-import { DeletePage } from "./pages/DeleteEntity/index";
-import { EditPage } from "./pages/EditEntity/index";
-import { ViewPage } from "./pages/ViewEntity";
+import { UserPage } from "./pages/ViewEntity";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
 import { LoginPage } from "./pages/Login/index";
@@ -13,9 +11,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<HomePage />} />
-      <Route path="/delete/:id" element={<DeletePage />} />
-      <Route path="/edit/:id" element={<EditPage />} />
-      <Route path="/view/:id" element={<ViewPage />} />
+      <Route path="user/:id" element={<UserPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
